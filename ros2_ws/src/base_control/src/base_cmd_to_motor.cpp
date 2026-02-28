f219130570eefb645de422de24c05562821a4635
keitaroi446 <149392536+keitaroi446@users.noreply.github.com>
2026-02-28 18:43:17 +0900

    update base_control to use BaseMotor msg

--- ros2_ws/src/base_control/src/base_cmd_to_motor.cpp
+++ ros2_ws/src/base_control/src/base_cmd_to_motor.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include "rclcpp/rclcpp.hpp"
 #include "redburi_msgs/msg/base_command.hpp"
+#include "redburi_msgs/msg/base_motor.hpp"
 
 class BaseCmdToMotor : public rclcpp::Node
 {
@@ -21,6 +22,7 @@ public:
         baseCmdCallback(msg);
       }
     );
+    motor_pub_ = create_publisher<redburi_msgs::msg::BaseMotor>("/base_motor", 10);
   }
 
 private:
@@ -30,58 +32,52 @@ private:
   double max_motor_rpm_{};
   double max_steer_deg_{};
   rclcpp::Subscription<redburi_msgs::msg::BaseCommand>::SharedPtr cmd_sub_;
+  rclcpp::Publisher<redburi_msgs::msg::BaseMotor>::SharedPtr motor_pub_;
   
   void baseCmdCallback(const redburi_msgs::msg::BaseCommand::SharedPtr msg)
   {
+    redburi_msgs::msg::BaseMotor base;
     double drive{msg->drive};
     double steer{msg->steer};
     double spin{msg->spin};
-    double steer_deg{};
-    double motor_f_rpm{};
-    double motor_r_rpm{};
-    double motor_l_rpm{};
 
     if(spin != 0)
     {
-      steer_deg = 90.0;
-      motor_f_rpm = max_spin_rpm_ * spin;
-      motor_r_rpm = motor_f_rpm * wheelbase_m_ / (tread_m_ / 2.0);
-      motor_l_rpm = -motor_r_rpm;
+      base.steer_deg = 90.0;
+      base.motor_f_rpm = max_spin_rpm_ * spin;
+      base.motor_r_rpm = base.motor_f_rpm * (tread_m_ / 2.0) / wheelbase_m_;
+      base.motor_l_rpm = -base.motor_r_rpm;
     }
     else if(drive != 0.0 || steer != 0.0)
     {
-      steer_deg = max_steer_deg_ * steer;
-      double steer_rad = std::fabs(steer_deg) * M_PI / 180.0; 
-      motor_f_rpm = max_motor_rpm_ * drive;
+      base.steer_deg = max_steer_deg_ * steer;
+      double steer_rad = std::fabs(base.steer_deg) * M_PI / 180.0; 
+      base.motor_f_rpm = max_motor_rpm_ * drive;
 
       if(steer == 0)
       {
-        motor_r_rpm = motor_f_rpm;
-        motor_l_rpm = motor_f_rpm;  
+        base.motor_r_rpm = base.motor_f_rpm;
+        base.motor_l_rpm = base.motor_f_rpm;  
       }
       else
       {
-        double outer_rpm = motor_f_rpm * (wheelbase_m_ / std::tan(steer_rad) + tread_m_ / 2.0) / (wheelbase_m_ / std::sin(steer_rad));
-        double inner_rpm = motor_f_rpm * (wheelbase_m_ / std::tan(steer_rad) - tread_m_ / 2.0) / (wheelbase_m_ / std::sin(steer_rad));
+        double outer_rpm = base.motor_f_rpm * (wheelbase_m_ / std::tan(steer_rad) + tread_m_ / 2.0) / (wheelbase_m_ / std::sin(steer_rad));
+        double inner_rpm = base.motor_f_rpm * (wheelbase_m_ / std::tan(steer_rad) - tread_m_ / 2.0) / (wheelbase_m_ / std::sin(steer_rad));
 
         if(steer > 0.0)
         {
-          motor_r_rpm = outer_rpm;
-          motor_l_rpm = inner_rpm;
+          base.motor_r_rpm = outer_rpm;
+          base.motor_l_rpm = inner_rpm;
         }
         else
         {
-          motor_r_rpm = inner_rpm;
-          motor_l_rpm = outer_rpm;
+          base.motor_r_rpm = inner_rpm;
+          base.motor_l_rpm = outer_rpm;
         }
       }
     }
-    
-    RCLCPP_INFO(
-      get_logger(),
-      "base_cmd drive=%.3f steer=%.3f spin=%.3f steer_deg=%.2f f=%.2f l=%.2f r=%.2f",
-      drive, steer, spin, steer_deg, motor_f_rpm, motor_l_rpm, motor_r_rpm
-    );
+
+    motor_pub_->publish(base);
   }
 };
 
